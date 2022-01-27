e03ead2b92a78fb41a838a934ad998cc677e1a23
Gabriel Seixas <salesseixasgabriel@gmail.com>
2022-01-27 02:34:55 -0300

    added show for SID

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect, useCallback } from 'react'
-import { Table } from 'antd'
-import SiDStatusService from './service'
+import { Table, TablePaginationConfig, Modal } from 'antd'
+import SiDStatusService, { PaginatedSIDStatus } from './service'
 import { SIDStatus } from './data'
 
 
@@ -28,18 +28,62 @@ const columns = [
 ]
 
 function App() {
-  const [sidStatus, setSidStatus] = useState<SIDStatus[]>([])
+  const [loading, setLoading] = useState(false)
+  const [paginated, setPaginated] = useState<PaginatedSIDStatus>({
+    data: [],
+    total: 0,
+    current: 1,
+    pageSize: 10,
+  })
+  const [currentSID, setCurrentSID] = useState<SIDStatus>({
+    integrationName: '',
+    key: '',
+    executionID: '',
+    status: '',
+  })
 
   useEffect(() => {
-    SiDStatusService.getAll(5, 0)
+    setLoading(true)
+    SiDStatusService.getAll(1, 5)
       .then(status => {
-        setSidStatus(status)
+        setPaginated(status)
+        setLoading(false)
+      })
+  }, [])
+
+  const handleTableChange = useCallback((pagination: TablePaginationConfig, _filters: any, _sorter: any) => {
+    let current = 0
+    if (pagination.current) { current = pagination.current }
+
+    setLoading(true)
+    SiDStatusService.getAll(current, pagination.pageSize)
+      .then(status => {
+        setPaginated(status)
+        setLoading(false)
       })
   }, [])
 
   return (
     <div className="App">
-      <Table columns={columns} dataSource={sidStatus} />
+      <Modal title="Basic Modal" visible={isModalVisible} onOk={handleOk} onCancel={handleCancel}>
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+      </Modal>
+      <Table
+        onRow={(record) => {
+          return {
+            onClick: () => {
+              console.log(record)
+            },
+          }
+        }}
+        columns={columns}
+        dataSource={paginated.data}
+        loading={loading}
+        pagination={paginated}
+        onChange={handleTableChange}
+      />
     </div>
   )
 }
--- src/service.ts
+++ src/service.ts
@@ -2,12 +2,30 @@ import axios from 'axios';
 import { data, SIDStatus } from './data';
 // import axios from 'axios';
 
+export interface PaginatedSIDStatus {
+  data: SIDStatus[];
+  total: number;
+  current: number;
+  pageSize: number;
+}
+
 const SIDStatusService = {
   // Implement axios calls here
-  async getAll(limit: number, offset: number): Promise<SIDStatus[]> {
+  async getAll(page: number, limit: number = 10): Promise<PaginatedSIDStatus> {
     // axios.get('http://localhost:3000/api/v1/sid-status', {})
     //   .then(response => {})  
-    return Promise.resolve(data.slice(offset, offset + limit));
+
+    const pageCount = (page - 1) * limit;
+    const limitCount = page * limit;
+    const result = data.slice(pageCount, limitCount);
+
+    // Just change this call to a axios with limit and offset
+    return Promise.resolve({
+      data: result,
+      total: data.length,
+      current: page,
+      pageSize: limit
+    });
   },
 
   async getByKey(key: string): Promise<SIDStatus | null> {
